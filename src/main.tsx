68dec972be59142ba311563ddbdd4d11f858a876
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-05-22 10:23:19 +0300

    update 65543

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,8 @@ import './index.css';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
